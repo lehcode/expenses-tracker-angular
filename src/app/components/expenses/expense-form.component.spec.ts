24d193171e1c33c20e31f734c66728de1ec461cb
lehcode <53556648+lehcode@users.noreply.github.com>
2024-12-06 08:54:46 +0200

    chore: Auto-format

--- src/app/components/expenses/expense-form.component.spec.ts
+++ src/app/components/expenses/expense-form.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+
 import { ExpensesFormComponent } from './expense-form.component'
 
 describe('ExpensesFormComponent', () => {
--- src/app/pages/edit-expense/edit-expense.page.spec.ts
+++ src/app/pages/edit-expense/edit-expense.page.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+
 import { EditExpenseComponent } from './edit-expense.page'
 
 describe('EditExpenseComponent', () => {
